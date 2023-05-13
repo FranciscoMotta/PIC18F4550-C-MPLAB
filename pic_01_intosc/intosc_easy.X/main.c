ec18e2a11132985904a33eb934fee77af79cb84c
Julio Francisco Motta Claudio <59737000+FranciscoMotta@users.noreply.github.com>
2023-05-12 21:45:58 -0500

    add intosc

---
+++ pic_01_intosc/intosc_easy.X/main.c
@@ -0,0 +1,51 @@
+/* 
+ * File:   main.c
+ * Author: Administrador
+ *
+ * Created on 12 de mayo de 2023, 07:58 PM
+ */
+
+#define _XTAL_FREQ 8000000ul
+
+/* Librerías */
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "system_config.h"
+
+/* Macros */
+
+/* Declaración de funciones */
+
+void Internal_Oscillator_Init (void);
+
+/*
+ * Main
+ */
+int main() 
+{
+    Internal_Oscillator_Init(); // Iniciamos el reloj intenro
+    TRISD &= ~(1 << _TRISD_TRISD0_POSITION); // LD0 como salida
+    LATD &= ~(1 << _LATD_LATD0_POSITION); // Led en LD0 apagado
+    while(true)
+    {
+        LATD ^= (1 << _LATD_LATD0_POSITION);
+        __delay_ms(500);
+    }
+    return (EXIT_SUCCESS);
+}
+
+/* Definición de funciones */
+
+void Internal_Oscillator_Init (void)
+{
+    /* Limpiamos el registro OSCCON */
+    OSCCON = 0x00;
+    /* Frecuencia de 8 bits */
+    OSCCON |= (0b111 << _OSCCON_IRCF0_POSITION); // OSCCON = 0B01110000
+    /* Internal Oscillator */
+    OSCCON |= (0b10 << _OSCCON_SCS0_POSITION); // OSCCON = 0B01110010
+}
