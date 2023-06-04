b0971deb7d7bfb25303596d4195a072217f1f5e1
Julio Francisco Motta Claudio <59737000+FranciscoMotta@users.noreply.github.com>
2023-06-03 21:06:14 -0500

    add codes timer0 polling

---
+++ pic_03_timers/timer0_polling.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Administrador
+ *
+ * Created on 3 de junio de 2023, 06:27 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
