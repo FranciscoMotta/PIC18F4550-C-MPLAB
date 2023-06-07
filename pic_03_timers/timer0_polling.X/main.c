3d324f31ffca7296ed3fc8ed594608a4ce03c2e2
Julio Francisco Motta Claudio <59737000+FranciscoMotta@users.noreply.github.com>
2023-06-06 19:53:16 -0500

    add codes

--- pic_03_timers/timer0_polling.X/main.c
+++ pic_03_timers/timer0_polling.X/main.c
@@ -5,14 +5,43 @@
  * Created on 3 de junio de 2023, 06:27 PM
  */
 
+#define _XTAL_FREQ 8000000UL
+
+/* Includes */
+#include <xc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "system_config.h"
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+/* Declaración de funciones */
 
+void Init_Internal_Oscillator (void);
+void Init_Timer_0_As_Timer (void);
+
+/* Main */
+int main(void)
+{
+    /* Se configura el oscilador interno */
+    Init_Internal_Oscillator();
+    /*  */
     return (EXIT_SUCCESS);
 }
 
+/* Definición de funciones */
+
+void Init_Timer_0_As_Timer (void)
+{
+    /* Limpiamos los registros */
+    T0CON = 0x00;
+    
+    /* Configuramos el timer0 a 1ms  */
+}
+void Init_Internal_Oscillator (void)
+{
+    /* Se limpian los registros */
+    OSCCON = 0x00;
+    
+    /* Configuramos el oscilador interno a 8Mhz */
+    OSCCONbits.IRCF = 0b111; // Freq 8mhz
+    OSCCONbits.SCS = 0b11; // Intosc
+}
